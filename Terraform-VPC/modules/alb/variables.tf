190b4e53bba9d340c3ae33d660149b70b6d848eb
Esene <peter.esene@informa.com>
2024-03-25 16:24:53 +0000

    adding terraform and github action files

---
+++ Terraform-VPC/modules/alb/variables.tf
@@ -0,0 +1,21 @@
+variable "sg_id" {
+  description = "SG ID for ALB"
+  type        = string
+
+}
+variable "subnets" {
+  description = "subnets for alb"
+  type        = list(string)
+
+}
+variable "vpc_id" {
+  description = "VPC ID for alb"
+  type        = string
+
+}
+
+variable "instances" {
+  description = "Instance ID for target group attachment"
+  type        = list(string)
+
+}
